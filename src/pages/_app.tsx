cc3ef0ac046b5b369b06827d8c24363a9b874b72
junimhs <junimhs10@gmail.com>
2021-11-23 11:59:26 -0300

    create styles global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
