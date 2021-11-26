57ce259fe0e64264f2991b23af96905a82831d38
junimhs <junimhs10@gmail.com>
2021-11-25 22:00:54 -0300

    create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 import type { NextPage } from "next";
+import styles from "./home.module.scss";
 
 const Home: NextPage = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>
+          News about the <span>React</span> world.
+        </h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+      <img src="/images/avatar.svg" alt="Girl coding" />
+    </main>
+  );
 };
 
 export default Home;
